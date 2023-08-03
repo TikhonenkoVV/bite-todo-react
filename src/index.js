c917df61b5b01a104fd24affb9ef2eb4065b64ac
tekostyshyn <tekostyshyn@gmail.com>
2023-08-03 13:38:20 +0300

    Bug with action fixed

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
-import { store, persistor } from './redux/store';
+import { store, persistor } from './store/store';
 import { App } from 'components/App';
 import './index.css';
 
