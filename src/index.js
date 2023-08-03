eaee9b61c760ac5a350162b61a69ec2d36253e6c
TikhonenkoVV <113926273+TikhonenkoVV@users.noreply.github.com>
2023-08-03 14:14:35 +0300

    add theme provider

--- src/index.js
+++ src/index.js
@@ -2,9 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
+import { Global, ThemeProvider } from '@emotion/react';
+import { GlobalStyles, theme } from 'styles';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <Global styles={GlobalStyles} />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
