ff9848dbdc62ad2cc0169ad38b0ddf3319ab6c61
RomaBortnik <bortnik25051998@gmail.com>
2023-08-03 17:56:52 +0300

    done

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -5,42 +5,44 @@ import Test from 'components/Test';
 
 const Welcome = () => {
   const { isModalOpen, closeModal, openModal } = useModal();
-  // return (
-
-  //   <div
-  //     style={{
-  //       height: '100vh',
-  //       display: 'flex',
-  //       flexDirection: 'column',
-  //       justifyContent: 'center',
-  //       alignItems: 'center',
-  //       fontSize: 60,
-  //       color: '#010101',
-  //     }}
-  //   >
-  //     <div
-  //       style={{
-  //         display: 'flex',
-  //         flexDirection: 'column',
-  //         justifyContent: 'center',
-  //         alignItems: 'center',
-  //         gap: 20,
-  //         fontSize: '14px',
-  //       }}
-  //     >
-  //       <img src={hero1x} alt="hero" />
-  //       <p>
-  //         Supercharge your productivity and take control of your tasks with Task
-  //         Pro - Don't wait, start achieving your goals now!
-  //       </p>
-  //     </div>
-  //     <button type="button" onClick={openModal}>
-  //       showModal
-  //     </button>
-  //     {isModalOpen && <Modal onClose={closeModal}><Test onClose={closeModal} /></Modal>}
-  //   </div>
-
-  // );
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 60,
+        color: '#010101',
+      }}
+    >
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          gap: 20,
+          fontSize: '14px',
+        }}
+      >
+        <img src={hero1x} alt="hero" />
+        <p>
+          Supercharge your productivity and take control of your tasks with Task
+          Pro - Don't wait, start achieving your goals now!
+        </p>
+      </div>
+      <button type="button" onClick={openModal}>
+        showModal
+      </button>
+      {isModalOpen && (
+        <Modal onClose={closeModal}>
+          <Test onClose={closeModal} />
+        </Modal>
+      )}
+    </div>
+  );
 };
 
 export default Welcome;
