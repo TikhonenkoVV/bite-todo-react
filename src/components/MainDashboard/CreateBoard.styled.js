e4224c9a48c195ec417f787a0d0e4243c117e986
kozhushna <k.kozhushna@gmail.com>
2023-08-05 15:55:37 +0300

    Add CreateBoard component

---
+++ src/components/MainDashboard/CreateBoard.styled.js
@@ -0,0 +1,21 @@
+import styled from '@emotion/styled';
+
+export const CreateBoardContainer = styled.div`
+  margin: auto;
+  text-align: center;
+  color: rgba(255, 255, 255, 0.5);
+  font-family: Poppins;
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 1.2;
+  letter-spacing: -0.28px;
+
+  @media (min-width: 768px) {
+    max-width: 486px;
+  }
+`;
+
+export const CreateBoardButton = styled.button`
+  color: #bedbb0;
+  font-family: Poppins;
+`;
--- src/pages/MainDashboard/MainDashboard.js
+++ src/pages/MainDashboard/MainDashboard.js
@@ -1,7 +1,9 @@
+import { useNavigate, useParams } from 'react-router-dom';
 import { useModal } from 'hooks/useModal';
 import { Modal } from 'components/Modal';
 import { ColumnForm } from '../../components/MainDashboard';
 import { ColumnList } from '../../components/MainDashboard/ColunmList';
+import CreateBoard from '../../components/MainDashboard/CreateBoard';
 import sprite from '../../img/icons/sprite.svg';
 import {
   MainDashboardContainer,
@@ -16,36 +18,42 @@ import {
 import DashboardHeader from 'components/DashboardHeader/DashboardHeader';
 
 const MainDashboard = () => {
+  const { boardName } = useParams();
+  console.log(boardName);
   const { isModalOpen, closeModal, openModal } = useModal();
   return (
-    <>
-      <MainDashboardSection>
-        {/* <FilterContainer>
+    <MainDashboardSection>
+      {/* <FilterContainer>
           <DashboardHeader name={'Test'} />
         </FilterContainer> */}
-        <MainDashboardContainer>
-          <MainDashboardSectionTitle>
-            Columns with tasks
-          </MainDashboardSectionTitle>
-          <ContentHolder>
-            <ColumnList />
-            <MainDashboardAddColumnButton type="button" onClick={openModal}>
-              <MainDashboardIconContainer>
-                <MainDashboardIcon>
-                  <use href={`${sprite}#icon-plus`}></use>
-                </MainDashboardIcon>
-              </MainDashboardIconContainer>
-              <div>Add another column</div>
-            </MainDashboardAddColumnButton>
-          </ContentHolder>
-          {isModalOpen && (
-            <Modal onClose={closeModal}>
-              <ColumnForm onCloseForm={closeModal} />
-            </Modal>
-          )}
-        </MainDashboardContainer>
-      </MainDashboardSection>
-    </>
+      <MainDashboardContainer>
+        {boardName ? (
+          <>
+            <MainDashboardSectionTitle>
+              Columns with tasks
+            </MainDashboardSectionTitle>
+            <ContentHolder>
+              <ColumnList />
+              <MainDashboardAddColumnButton type="button" onClick={openModal}>
+                <MainDashboardIconContainer>
+                  <MainDashboardIcon>
+                    <use href={`${sprite}#icon-plus`}></use>
+                  </MainDashboardIcon>
+                </MainDashboardIconContainer>
+                <div>Add another column</div>
+              </MainDashboardAddColumnButton>
+            </ContentHolder>
+            {isModalOpen && (
+              <Modal onClose={closeModal}>
+                <ColumnForm onCloseForm={closeModal} />
+              </Modal>
+            )}
+          </>
+        ) : (
+          <CreateBoard />
+        )}
+      </MainDashboardContainer>
+    </MainDashboardSection>
   );
 };
 
