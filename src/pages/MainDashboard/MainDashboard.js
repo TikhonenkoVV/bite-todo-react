5abab92b1ee7a6703036fe01e5965980775006e5
kozhushna <k.kozhushna@gmail.com>
2023-08-05 16:02:41 +0300

    Update MainDashboard

--- src/pages/MainDashboard/MainDashboard.js
+++ src/pages/MainDashboard/MainDashboard.js
@@ -24,7 +24,7 @@ const MainDashboard = () => {
   return (
     <MainDashboardSection>
       <FilterContainer>
-        <DashboardHeader name={boardName} />
+        <DashboardHeader name={boardName} theme={'dark'} />
       </FilterContainer>
       <MainDashboardContainer>
         {boardName ? (
