33fadd00d25ce33ab13d1c0c9cdb16817d815ac5
TikhonenkoVV <113926273+TikhonenkoVV@users.noreply.github.com>
2023-08-07 15:18:07 +0300

    add AddTaskForm

---
+++ src/components/AddTaskForm/AddTaskForm.jsx
@@ -0,0 +1,231 @@
+import React, { useState, useRef } from 'react';
+import { useFormik } from 'formik';
+import styled from '@emotion/styled';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import moment from 'moment';
+import * as Yup from 'yup';
+import sprite from '../../img/icons/sprite.svg';
+import 'react-datepicker/dist/react-datepicker.css';
+import './AddTaskForm.css';
+import { useDispatch } from 'react-redux';
+import { addTask } from '../../store/card/operation';
+import {
+  StyledP,
+  StyledDiv,
+  StyledInput,
+  StyledTextArea,
+  StyledButton,
+  RadioGroup,
+  RadioInput,
+  RadioLabel,
+  StyledTitelBtn,
+  StyledTitleDeadline,
+  Container,
+  StyledFormikTitleNotification,
+  StyledFormikDescriptionNotification,
+  StyledBtnClose,
+  PlusIconContainer,
+  StyledFormikColorNotification,
+} from './AddTaskForm.styled';
+
+export const colors = ['#8FA1D0', '#E09CB5', '#BEDBB0', '#808080'];
+
+const StyledCustomCalendar = styled(DatePicker)`
+  &.custom-datepicker {
+    .react-datepicker-wrapper & .react-datepicker__day--keyboard-selected,
+    .react-datepicker-wrapper & .react-datepicker__day--today,
+    .react-datepicker-wrapper & .react-datepicker__day--weekend {
+      background-color: red !important;
+      color: white !important;
+    }
+
+    .react-datepicker-wrapper & .react-datepicker__day--selected {
+      background-color: red;
+      color: white;
+    }
+  }
+`;
+
+const initialValues = {
+  title: '',
+  description: '',
+  color: '',
+};
+
+export const AddTasks = ({ columnId, taskData }) => {
+  const [deadline, setDeadline] = useState('');
+
+  const dispatch = useDispatch();
+
+  const deadlinePickerRef = useRef(null);
+
+  const validationSchema = Yup.object().shape({
+    title: Yup.string()
+      .required('Title is required')
+      .min(3, 'Title must contain at least 3 characters')
+      .max(50, 'Title must not exceed 50 characters'),
+    description: Yup.string()
+      .required('Description is required')
+      .min(10, 'Description must contain at least 10 characters')
+      .max(500, 'Description must not exceed 500 characters'),
+    color: Yup.string().required('Please select a color'),
+    deadline: Yup.date()
+      .nullable()
+      .required('Please select a deadline date')
+      .min(new Date(), 'Deadline cannot be earlier than today'),
+  });
+
+  const handleDeadlineClick = () => {
+    if (deadlinePickerRef.current) {
+      deadlinePickerRef.current.setOpen(true);
+    }
+  };
+
+  const CurrentDate = () => {
+    const formattedDate = moment().format('MMMM D');
+    return <div>Today, {formattedDate}</div>;
+  };
+
+  const formattedDeadline = deadline
+    ? moment(deadline).format('D MMMM YYYY')
+    : CurrentDate();
+
+  const handleSubmit = async (values, { resetForm }) => {
+    try {
+      await dispatch(addTask(columnId, values));
+      console.log('Task added successfully!');
+      resetForm();
+      setDeadline('');
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  const formik = useFormik({
+    initialValues: initialValues,
+    onSubmit: handleSubmit,
+    validationSchema: validationSchema,
+  });
+
+  const handleDateChange = date => {
+    if (date && date < new Date()) {
+      alert('Deadline cannot be before today.');
+      return;
+    }
+    formik.setFieldValue('deadline', date);
+    setDeadline(date);
+  };
+
+  return (
+    <StyledDiv>
+      <StyledP>Add Card</StyledP>
+
+      <StyledBtnClose>
+        <svg
+          className="icon"
+          width="18"
+          height="18"
+          aria-hidden="true"
+          role="presentation"
+          fill="white"
+        >
+          <use xlinkHref={`${sprite}#icon-x-close`} />
+        </svg>
+      </StyledBtnClose>
+      <form onSubmit={formik.handleSubmit}>
+        <StyledInput
+          type="text"
+          name="title"
+          placeholder="Title"
+          value={formik.values.title}
+          onChange={formik.handleChange}
+        />
+        {formik.touched.title && formik.errors.title ? (
+          <StyledFormikTitleNotification>
+            {formik.errors.title}
+          </StyledFormikTitleNotification>
+        ) : null}
+        <StyledTextArea
+          type="text"
+          name="description"
+          placeholder="Description"
+          value={formik.values.description}
+          onChange={formik.handleChange}
+        />
+        {formik.touched.description && formik.errors.description ? (
+          <StyledFormikDescriptionNotification>
+            {formik.errors.description}
+          </StyledFormikDescriptionNotification>
+        ) : null}
+        <StyledTitelBtn>Label color</StyledTitelBtn>
+        <br />
+        <RadioGroup>
+          {colors.map((color, index) => (
+            <RadioLabel key={index}>
+              <RadioInput
+                type="radio"
+                name="color"
+                value={color}
+                checked={formik.values.color === color}
+                onChange={formik.handleChange}
+              />
+            </RadioLabel>
+          ))}
+        </RadioGroup>
+        {formik.touched.color && formik.errors.color ? (
+          <StyledFormikColorNotification>
+            {formik.errors.color}
+          </StyledFormikColorNotification>
+        ) : null}
+
+        <StyledTitleDeadline>
+          Deadline
+          <Container>
+            {formattedDeadline}
+            <svg
+              className="icon"
+              width="14"
+              height="14"
+              aria-hidden="true"
+              role="presentation"
+              fill="#BEDBB0"
+              onClick={handleDeadlineClick}
+              style={{
+                marginLeft: '5px',
+                cursor: 'pointer',
+              }}
+            >
+              <use xlinkHref={`${sprite}#icon-chevron-down`} />
+            </svg>
+          </Container>
+          <StyledCustomCalendar
+            className="custom-datepicker"
+            ref={deadlinePickerRef}
+            name="deadline"
+            selected={formik.values.deadline}
+            onChange={handleDateChange}
+            locale="en"
+            dateFormat="d MMMM yyyy"
+            customInput={<div></div>}
+          />
+        </StyledTitleDeadline>
+        <StyledButton type="submit">
+          <PlusIconContainer>
+            <svg
+              className="icon"
+              width="14"
+              height="14"
+              aria-hidden="true"
+              role="presentation"
+              fill="white"
+            >
+              <use xlinkHref={`${sprite}#icon-plus`} />
+            </svg>
+          </PlusIconContainer>
+          Add
+        </StyledButton>
+      </form>
+    </StyledDiv>
+  );
+};
---
+++ src/components/AddTaskForm/AddTaskForm.styled.js
@@ -0,0 +1,167 @@
+import styled from '@emotion/styled';
+
+export const StyledDiv = styled.div`
+  width: 350px;
+  height: 522px;
+  background-color: #151515;
+  margin-top: 10px;
+  margin-left: 10px;
+  border-radius: 10px;
+`;
+
+export const StyledBtnClose = styled.button`
+  width: 18px;
+  height: 18px;
+  margin-left: 212px;
+`;
+
+export const StyledP = styled.p`
+  display: inline-block;
+  color: #ffffff;
+  font-size: 18px;
+  margin-bottom: 24px;
+  margin-top: 24px;
+  margin-left: 24px;
+`;
+
+export const StyledInput = styled.input`
+  border-radius: 8px;
+  border: 1px solid #bedbb0;
+  opacity: 0.4;
+  background: #1f1f1f;
+  box-shadow: 0px 4px 16px 0px rgba(22, 22, 22, 0.08);
+  margin-bottom: 10px;
+  margin-left: 20px;
+  padding: 14px 115px 14px 18px;
+  font-size: 14px;
+  border-radius: 4px;
+  color: #ffffff;
+`;
+
+export const StyledTextArea = styled.textarea`
+  resize: none;
+  margin-left: 20px;
+  color: #ffffff;
+  display: flex;
+  padding: 14px 113px 122px 18px;
+  align-items: center;
+  flex-shrink: 0;
+  border-radius: 8px;
+  border: 1px solid #bedbb0;
+  opacity: 0.4;
+  background: #1f1f1f;
+  box-shadow: 0px 4px 16px 0px rgba(22, 22, 22, 0.08);
+  margin-bottom: 24px;
+`;
+
+export const StyledTitelBtn = styled.label`
+  color: rgba(255, 255, 255, 50%);
+  font-size: 12px;
+  margin-left: 24px;
+`;
+
+export const StyledButton = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-left: 24px;
+  width: 302px;
+  padding: 10px 0px 11px 0px;
+  justify-content: center;
+  align-items: center;
+  border-radius: 8px;
+  background: #bedbb0;
+`;
+
+export const Container = styled.div`
+  color: #bedbb0;
+  font-family: Poppins;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+  letter-spacing: -0.28px;
+  color: #bedbb0;
+  margin-left: 24px;
+  justify-content: left;
+  align-items: center;
+  display: flex;
+`;
+
+export const RadioGroup = styled.div`
+  display: flex;
+  align-items: center;
+  margin-left: 15px;
+`;
+
+export const RadioLabel = styled.label`
+  display: flex;
+  align-items: center;
+  margin-left: 10px;
+  cursor: pointer;
+  margin-top: 10px;
+`;
+
+export const RadioInput = styled.input`
+  margin-bottom: 14px;
+
+  &::before {
+    content: '';
+    display: inline-block;
+    border-radius: 50%;
+    width: ${({ checked }) => (checked ? '10px' : '14px')};
+    height: ${({ checked }) => (checked ? '10px' : '14px')};
+    border: ${({ checked, colors }) => (checked ? '1px' : '0px')} solid
+      ${({ colors }) => colors};
+    background-color: ${({ checked, value }) => (checked ? value : value)};
+    transition: width 0.2s ease, height 0.2s ease; /* Добавляем transition */
+  }
+  &:checked::before {
+    margin-bottom: 3.2px;
+    margin-left: 1.5px;
+    background-color: ${({ checked, value }) => (checked ? value : value)};
+  }
+`;
+
+export const PlusIconContainer = styled.div`
+  width: 28px;
+  height: 28px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-right: 8px;
+  background-color: #161616;
+  border-radius: 20%;
+`;
+
+export const StyledTitleDeadline = styled.span`
+  color: rgba(255, 255, 255, 0.5);
+  font-family: Poppins;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  letter-spacing: -0.24px;
+  margin-left: 24px;
+`;
+
+export const StyledFormikTitleNotification = styled.div`
+  margin-left: 24px;
+  margin-bottom: 5px;
+  color: red;
+  font-size: 12px;
+`;
+
+export const StyledFormikDescriptionNotification = styled.div`
+  margin-top: -15px;
+  margin-left: 24px;
+  color: red;
+  font-size: 12px;
+`;
+
+export const StyledFormikColorNotification = styled.div`
+  margin-top: -5px;
+  margin-left: 24px;
+  color: red;
+  font-size: 12px;
+`;
