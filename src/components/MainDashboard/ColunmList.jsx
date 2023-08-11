bfab9f9c7bf5a4243942d105b3931b36561fd8f7
kozhushna <k.kozhushna@gmail.com>
2023-08-11 11:34:50 +0300

    Add loader

--- src/components/MainDashboard/ColunmList.jsx
+++ src/components/MainDashboard/ColunmList.jsx
@@ -2,12 +2,14 @@ import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Column } from '../MainDashboard/Column';
 import { ColumnListStyled } from './ColunmList.styled';
-import { selectColumns } from 'store/columns/selectors';
+import { selectColumns, selectIsLoading } from 'store/columns/selectors';
 import { getColumns } from 'store/columns/operations';
+import { Loader } from '../Loader/Loader';
 
 export const ColumnList = ({ boardId }) => {
   const dispatch = useDispatch();
   const columns = useSelector(selectColumns);
+  const isLoading = useSelector(selectIsLoading);
 
   useEffect(() => {
     if (!boardId) {
@@ -16,6 +18,9 @@ export const ColumnList = ({ boardId }) => {
     dispatch(getColumns(boardId));
   }, [boardId, dispatch]);
 
+  if (isLoading) {
+    return <Loader fill={'#fff'} />;
+  }
   return (
     <ColumnListStyled>
       {columns.map(({ _id, title, createdAt, owner, cards }) => {
