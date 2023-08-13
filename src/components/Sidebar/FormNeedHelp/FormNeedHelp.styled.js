5b8bbf2debd1e7865953aa645207323ab2c63297
sps-pashka-it <stetsyuk.p.s@gmail.com>
2023-08-13 17:11:46 +0300

    finish styled modal need help

--- src/components/Sidebar/FormNeedHelp/FormNeedHelp.styled.js
+++ src/components/Sidebar/FormNeedHelp/FormNeedHelp.styled.js
@@ -22,7 +22,6 @@ export const StyledDiv = styled.div`
 `;
 
 export const StyledP = styled.p`
-  /* color: #ffffff; */
   font-size: 18px;
   letter-spacing: -0.02em;
 `;
@@ -35,17 +34,17 @@ export const StyledInput = styled.input`
   margin: 24px 0 14px 0;
 
   border-radius: 8px;
-  border: 1px solid #bedbb0;
-  background: #1f1f1f;
+  border: 1px solid ${props => props.theme.colors.formAddButtonBackground};
+  background: ${props => props.theme.colors.formInputBackground};
   opacity: 0.4;
   box-shadow: 0px 4px 16px 0px rgba(22, 22, 22, 0.08);
 
-  color: #fff;
+  color: ${props => props.theme.colors.formTitleColor};
   letter-spacing: -0.02em;
 
   &:focus {
     opacity: 1;
-    border: 1px solid #bedbb0;
+    border: 1px solid ${props => props.theme.colors.formAddButtonBackground};
   }
 `;
 
@@ -55,18 +54,19 @@ export const StyledTextArea = styled.textarea`
   height: 120px;
   padding: 14px 18px;
 
+  resize: none;
   border-radius: 8px;
-  border: 1px solid #bedbb0;
-  background: #1f1f1f;
+  border: 1px solid ${props => props.theme.colors.formAddButtonBackground};
+  background: ${props => props.theme.colors.formInputBackground};
   opacity: 0.4;
   box-shadow: 0px 4px 16px 0px rgba(22, 22, 22, 0.08);
 
-  color: #fff;
+  color: ${props => props.theme.colors.formTitleColor};
   letter-spacing: -0.02em;
 
   &:focus {
     opacity: 1;
-    border: 1px solid #bedbb0;
+    border: 1px solid ${props => props.theme.colors.formAddButtonBackground};
   }
 `;
 
@@ -77,8 +77,8 @@ export const StyledButton = styled.button`
   justify-content: center;
   align-items: center;
   border-radius: 8px;
-  background: #bedbb0;
-  color: #161616;
+  background: ${props => props.theme.colors.formAddButtonBackground};
+  color: ${props => props.theme.colors.formAddButtonColor};
   font-weight: 500;
   font-size: 14px;
   letter-spacing: -0.02em;
@@ -86,13 +86,13 @@ export const StyledButton = styled.button`
 
   &:hover,
   &:focus {
-    background: #9dc888;
+    background: ${props => props.theme.colors.formAddButtonHover};
   }
 `;
 
 export const DivIconClose = styled.div`
   position: absolute;
-  color: white;
+  color: ${props => props.theme.colors.formTitleColor};
   top: 14px;
   right: 14px;
   cursor: pointer;
