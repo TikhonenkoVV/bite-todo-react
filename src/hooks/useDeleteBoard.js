0e4153a21e3026400a61dfabc4a0f787f72f4747
sps-pashka-it <stetsyuk.p.s@gmail.com>
2023-08-19 13:43:54 +0300

    add AskDeleteModul Column

--- src/hooks/useDeleteBoard.js
+++ src/hooks/useDeleteBoard.js
@@ -1,5 +1,6 @@
 import { useSelector } from 'react-redux';
 import { selectBoardsState } from 'store/boards/selectors';
+import { selectColumns } from 'store/columns/selectors';
 
 export const useDeleteBoard = id => {
   const { boards } = useSelector(selectBoardsState);
@@ -7,5 +8,14 @@ export const useDeleteBoard = id => {
 
   return {
     isDeleteBoard,
+  };
+};
+
+export const useDeleteColumn = id => {
+  const columns = useSelector(selectColumns);
+  const isDeleteColumn = columns.find(({ _id }) => _id === id);
+
+  return {
+    isDeleteColumn,
   };
 };
