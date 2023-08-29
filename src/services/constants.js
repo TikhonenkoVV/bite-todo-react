98427313fd53f7bf08548f5d1435e7bdc46b99a8
Pavlo <113455386+sps-paska-it@users.noreply.github.com>
2023-08-29 14:36:29 +0200

    change base_url

--- src/services/constants.js
+++ src/services/constants.js
@@ -1,2 +1,2 @@
-export const BASE_URL = 'https://bite-todo-rest-api.onrender.com';
-// export const BASE_URL = 'http://localhost:5050';
+// export const BASE_URL = 'https://bite-todo-rest-api.onrender.com';
+export const BASE_URL = 'http://localhost:5050';
